eb127c8f303bac7998a04975c14c787aa60ef13a
Sushant <sushantshekhar01011999@gmail.com>
2025-01-03 19:46:50 +0530

    Prod failed

---
+++ src/component/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+
+
+export default function Footer() {
+  return (
+ 
+    <Box sx={{height:'60px'}}>
+    <Box sx={{ display:'flex', alignItems:'center', justifyContent:'space-between', padding:2 }}>
+       <Box sx={{color:'yellow'}}>
+        BODSPHERE
+       </Box>
+       <Box sx={{display:'flex', alignItems:'center', justifyContent:'flex-end', gap:4 }} >
+        <Link  to="/" style={{ textDecoration: 'none', color: 'inherit' }}>Home</Link>
+        <Link to="/categories" style={{ textDecoration: 'none', color: 'inherit' }}>Categories</Link>
+        <Link  to="/notification" style={{ textDecoration: 'none', color: 'inherit' }}>Notification</Link>
+        <Link  to="/aimusic" style={{ textDecoration: 'none', color: 'inherit' }}>Ai Music</Link>
+       </Box>
+    </Box>
+</Box>
+  )
+}
--- src/component/Home.js
+++ src/component/Home.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Box from '@mui/material/Box';
 import Navbar from './Navbar';
 import yogapic from "../image/yogapic.jpg";
-import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
+import Footer from './Footer';
 
 
 export default function Home() {
@@ -22,12 +22,12 @@ export default function Home() {
                         position: 'relative',
                     }}
                 >
-                    {/* Text overlay with better width and padding */}
                     <Box sx={{
                         wordBreak: 'break-word',
-                        width: '40%', // Adjust width as necessary
+                        width: '40%',
                         padding: 5,
-                        backgroundColor: 'rgba(0, 0, 0, 0.5)', // Optional: for better text visibility on image
+                        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+
                         color: 'white',
                         position: 'absolute',
                         top: '50%',
@@ -40,17 +40,14 @@ export default function Home() {
                         <h4>Everything under $10</h4>
                     </Box>
                 </Box>
-
-                {/* Statistics Section */}
                 <Box sx={{
                     display: 'flex',
                     justifyContent: 'space-around',
                     alignItems: 'center',
                     padding: 4,
-                    flexWrap: 'wrap', // Makes the layout responsive on smaller screens
+                    flexWrap: 'wrap',
                     backgroundColor: '#f0f0f0',
                 }}>
-                    {/* Box for Countries */}
                     <Box sx={{
                         display: 'flex',
                         alignItems: 'center',
@@ -62,8 +59,6 @@ export default function Home() {
                         <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>190+</p>
                         <p>Countries</p>
                     </Box>
-
-                    {/* Box for On-Demand Classes */}
                     <Box sx={{
                         display: 'flex',
                         alignItems: 'center',
@@ -75,8 +70,6 @@ export default function Home() {
                         <p style={{ fontSize: '2rem', fontWeight: 'bold' }}>1000+</p>
                         <p>On-Demand Classes</p>
                     </Box>
-
-                    {/* Box for Members */}
                     <Box sx={{
                         display: 'flex',
                         alignItems: 'center',
@@ -95,12 +88,11 @@ export default function Home() {
                     <Box style={{ fontSize: 40, fontWeight: 'bold' }}>Discover Your Yogic Path</Box>
                     <Box style={{ fontSize: 20 }}>Join our community and transform your yoga practise today.</Box>
                 </Box>
-
                 <Box sx={{
                     display: 'flex', alignItems: 'center', justifyContent: 'center', gap: 4, padding: 4., backgroundColor: '#f0f0f0', boxShadow: '5px 5px 15px rgba(255, 47, 47, 0.3)'
                 }}>
                     <Box sx={{
-                        height: '100%', width: '100%', border: '2px solid black', textAlign: 'center', // Adding a white border
+                        height: '100%', width: '100%', border: '2px solid black', textAlign: 'center',
 
                     }}>                    <CardContent>
                             <Typography gutterBottom sx={{ color: 'black', fontSize: 14, fontWeight: 'bold' }}>
@@ -115,7 +107,6 @@ export default function Home() {
                         <CardActions>
                         </CardActions>
                     </Box>
-
                     <Box sx={{ height: '100%', width: '100%', border: '2px solid black', textAlign: 'center', }}>                    <CardContent>
                         <Typography gutterBottom sx={{ color: 'black', fontWeight: 'bold', fontSize: 14 }}>
                             Yoga Classes
@@ -136,28 +127,28 @@ export default function Home() {
                         <Typography variant="h5" component="div">
                         </Typography>
                         <Typography variant="body2">
-Experience tranquility with our calming music, perfect for relaxation and mindfulness and done and done again
+                            Experience tranquility with our calming music, perfect for relaxation and mindfulness and done and done again
                         </Typography>
                     </CardContent>
                         <CardActions>
                         </CardActions>
                     </Box>
                     <Box sx={{ height: '100%', width: '100%', border: '2px solid black', textAlign: 'center', }}>                    <CardContent>
-                        <Typography gutterBottom sx={{ color: 'black', fontWeight:'bold', fontSize: 14 }}>
-Pranayam & Meditation
+                        <Typography gutterBottom sx={{ color: 'black', fontWeight: 'bold', fontSize: 14 }}>
+                            Pranayam & Meditation
                         </Typography>
                         <Typography variant="h5" component="div">
                         </Typography>
                         <Typography variant="body2">
-                          Explore  the power of breath and enhance your self-awareness and inner peace and done  and done and done
+                            Explore  the power of breath and enhance your self-awareness and inner peace and done  and done and done
                         </Typography>
                     </CardContent>
                         <CardActions>
                         </CardActions>
                     </Box>
                 </Box>
-
             </Box>
+            <Footer/>
         </>
     );
 }
